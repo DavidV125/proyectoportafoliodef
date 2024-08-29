f9cbdcfb682c5dbdd16df1be189b315cb3aa1117
DavidV125 <davvillacura@gmail.com>
2024-08-28 21:20:58 -0400

    version 6

---
+++ assets/js/logica.js
@@ -0,0 +1,45 @@
+function mostrarExperiencia() {
+    let elParrafo = document.getElementById("especial");
+    elParrafo.style.color = "black";
+    elParrafo.style.backgroundColor = "orange";
+    elParrafo.style.fontSize = "15px";
+    elParrafo.innerHTML = `
+        <ul>
+            <li>
+                <b>Flip-Class (Marzo 2022 – presente):</b><br>
+                Cursos de inglés nivel básico y medio (A1-C1) en modalidad online. Cursos individuales o grupales en empresas.
+            </li>
+            <br>
+            <li>
+                <b>Universidad Diego Portales (Marzo 2022 – Julio 2022):</b><br>
+                Ayudantía para Fundamentos de Finanzas de la carrera de Contador Auditor Vespertino, profesor Francisco León Heyder.
+            </li>
+            <br>
+            <li>
+                <b>Geasd Language Community (Mayo 2019 – Junio 2021):</b><br>
+                Cursos de inglés nivel básico y medio (A1-A2, B1-B2) en modalidades online y presencial para cursos individuales y/o grupales en empresas.
+            </li>
+            <br>
+            <li>
+                <b>DuocUC Instituto Profesional (2012 – 2019):</b><br>
+                Para cursos transversales: inglés nivel básico, medio y avanzado, y cursos de inglés específicos para las carreras de Comercio Exterior, Finanzas, Publicidad, Marketing y Logística.
+            </li>
+            <br>
+        </ul>
+    `;
+}
+
+function mostrarAntecedentes() {
+    let elParrafo = document.getElementById("especial");
+    elParrafo.style.color = "black";
+    elParrafo.style.backgroundColor = "orange";
+    elParrafo.style.fontSize = "15px";
+    elParrafo.innerHTML = `
+        <ul>
+            <li><b>Universidad San Sebastián, Santiago 2023</b> - Ingeniería en Administración de Empresas Mención Control de Gestión</li>
+            <br>
+            <li><b>Universidad Chileno-Británico de Cultura, Santiago 2012</b> – Título Profesional de Pedagogía en Inglés</li>
+                    
+        </ul>
+    `;
+}
